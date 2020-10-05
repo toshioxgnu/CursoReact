77ebb2517f9d5df67ddd8e59b2e5bd57d66521cb
Jose Gonzalez Riquelme <jgonzalezr5@outlook.es>
2020-10-05 10:38:35 -0300

    Clase 2 bases de js

---
+++ 02-intro-javascript/src/bases/01-const-let.js
@@ -0,0 +1,12 @@
+const nombre = 'Fernando';
+const apellido = 'Herrera';
+
+let valorDado = 5;
+valorDado = 6;
+
+console.log(nombre,apellido,valorDado);
+
+if(true){
+    let valorDado = 4;
+    console.log(valorDado);
+}
