f46be2ff3c82905888b8756e679985427909f6e4
Jose Gonzalez Riquelme <jgonzalezr5@outlook.es>
2021-04-12 23:52:42 -0400

    Intro-Javascript

---
+++ 02-intro-javascript/src/bases/11-async-await.js
@@ -0,0 +1,21 @@
+const getImage = async () => {
+    try{
+        const apiKey = 'mGT4mPb0wmbp0Dz1ZXdTlFQpoHWRrah0';
+
+        const resp = await fetch(`http://api.giphy.com/v1/gifs/random?api_key=${apiKey}`);
+
+        const {data} = await resp.json();
+        const { url } = data.images.original;
+
+        const img = document.createElement('img');
+        img.src = url;
+        document.body.append( img );
+
+    }catch(error){
+        //Manejo error
+        console.err(error);
+    }
+} 
+
+getImage();
+
