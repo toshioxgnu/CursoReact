d0e9720cd118ea4e1defc64e75e11bc5e4c82739
jose gonzalez riquelme <jgonzalezr5@outlook.es>
2021-05-13 12:08:12 -0400

    Se terminan pruebas unitarias

---
+++ 03-counter-app/src/tests/base/11-async-await.test.js
@@ -0,0 +1,11 @@
+import { getImagen } from "../../base/11-async-await"
+
+describe(' Pruebas con async await', () => {
+
+    test('Debe retornar el url de la imagen', async() => {
+        const url = await getImagen();
+
+        expect( url.includes('https://') ).toBe( true );
+    })
+    
+})
