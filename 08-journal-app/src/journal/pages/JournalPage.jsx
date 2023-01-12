657091ba2a7e1df7706efcb7a4ae6fe2330f5c04
Jose Gonzalez Riquelme <jgonzalezr5@outlook.es>
2023-01-12 11:20:13 -0300

    Termino layout general de la app

--- 08-journal-app/src/journal/pages/JournalPage.jsx
+++ 08-journal-app/src/journal/pages/JournalPage.jsx
@@ -1,5 +1,7 @@
+import { IconButton } from "@mui/material"
 import { JournalLayout } from "../layout/JournalLayout"
 import { NoteView, NothingSelectedView } from "../views"
+import {  AddOutlined } from "@mui/icons-material"
 
 
 
@@ -12,6 +14,22 @@ export const JournalPage = () => {
 
       {/* <NothingSelectedView /> */}
       <NoteView />
+
+      <IconButton 
+        size="large"
+        sx={{
+          color: 'white',
+          backgroundColor: 'error.main',
+          ':hover' : { backgroundColor: 'error.main', opacity: 0.9 },
+          position: 'fixed',
+          right: 50,
+          bottom: 50,
+        }}
+      >
+        <AddOutlined 
+          sx={{ fontSize: 30 }}
+        />
+      </IconButton>
 
     </JournalLayout>
 
