b5abc9af94734b903fa60cc32787f3df70bc034c
Jose Gonzalez Riquelme <jgonzalezr5@outlook.es>
2023-01-14 16:32:25 -0300

    Se implementa llamado en pokemonApp

--- 09-toolkit-redux/src/PokemonApp.jsx
+++ 09-toolkit-redux/src/PokemonApp.jsx
@@ -1,32 +1,38 @@
 import { useEffect } from 'react'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { getPokemons } from './store/slices/pokemon';
 
 export const PokemonApp = () => {
 
   const dispatch = useDispatch();
+  const { isLoading, pokemons = [], page } = useSelector( state => state.pokemons );
 
 
   useEffect(() => {
     dispatch( getPokemons() );
-    
-    return () => {
-      
-    }
-  }, [])
-  
+
+  }, [])  
 
 
   return (
     <>
         <h1> Pokemon App </h1>
         <hr/>
-
+        <span> Loading: { !isLoading ? 'True' : 'False' } </span>
         <ul>
-            <li>hola</li>
-            <li>hola</li>
-            <li>hola</li>
+          {
+            pokemons.map( pokemon => (
+              <li key={ pokemon.name }>{ pokemon.name }</li>
+            ))
+          }
         </ul>
+
+        <button
+          disabled={ isLoading }
+          onClick={ () => dispatch( getPokemons(page) ) }
+        >
+          Next
+        </button>
     </>
   )
 }
