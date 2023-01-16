f7de2e89a18f273ba8db343c9f71264c8764f183
Jose Gonzalez Riquelme <jgonzalezr5@outlook.es>
2023-01-16 13:54:15 -0300

    Se Configura el authSlice.js

--- 08-journal-app/src/main.jsx
+++ 08-journal-app/src/main.jsx
@@ -1,13 +1,18 @@
+import './styles.css'
+import { BrowserRouter } from 'react-router-dom'
+import { JournalApp } from './journalApp'
+import { Provider } from 'react-redux';
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './styles.css'
-import { JournalApp } from './journalApp'
-import { BrowserRouter } from 'react-router-dom'
+import { store } from './store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <JournalApp />
-    </BrowserRouter>
+    <Provider store={ store } >
+      <BrowserRouter>
+        <JournalApp />
+      </BrowserRouter>
+    </Provider>
+    
   </React.StrictMode>,
 )
