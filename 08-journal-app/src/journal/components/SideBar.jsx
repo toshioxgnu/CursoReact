b8750be118aa9606ab6db80154dbbf4779256550
Jose Gonzalez Riquelme <jgonzalezr5@outlook.es>
2023-02-02 13:25:05 -0300

    se llevan las notas al sidebar desde firebase

--- 08-journal-app/src/journal/components/SideBar.jsx
+++ 08-journal-app/src/journal/components/SideBar.jsx
@@ -1,11 +1,13 @@
-import { TurnedInNot } from "@mui/icons-material"
-import { Box, Divider, Drawer, Grid, List, ListItemButton, ListItemIcon, ListItemText, Toolbar, Typography } from "@mui/material"
+import { Box, Divider, Drawer, List, Toolbar, Typography } from "@mui/material"
+import React from "react";
 import { useSelector } from "react-redux"
+import { SideBarItem } from "./SideBarItem";
 
 export const SideBar = ({ drawerWidth }) => {
 
     const {displayName } = useSelector( state => state.auth );
-
+    const { notes } = useSelector( state => state.journal );
+    
 
   return (
 
@@ -39,18 +41,9 @@ export const SideBar = ({ drawerWidth }) => {
 
             <List >
                 {
-                    ['enero', 'febrero','Marzo','Abril'].map( text => (
-                        <ListItemButton key={text}>
-                            <ListItemIcon>
-                                <TurnedInNot />
-                            </ListItemIcon>
-                            <Grid
-                                container
-                            >
-                                <ListItemText primary={text} />
-                                <ListItemText secondary={'Voluptate nulla deserunt cupidatat magna nisi aliquip officia duis commodo commodo labore ex.'} />
-                            </Grid>
-                        </ListItemButton>
+                    notes.map( note => (
+                        <SideBarItem key={ note.id } { ...note } />
+                        
                     ))
                 }
             </List>
---
+++ 08-journal-app/src/journal/components/SideBarItem.jsx
@@ -0,0 +1,26 @@
+import { TurnedInNot } from '@mui/icons-material'
+import { Grid, ListItem, ListItemButton, ListItemIcon, ListItemText } from '@mui/material'
+import React, { useMemo } from 'react'
+
+export const SideBarItem = ({ title= '', body ='', id }) => {
+
+    const newTitle = useMemo(() => {
+        return title.length > 17 ? title.substring(0,17) + '...' : title ;
+    }, [title])
+
+  return (
+    <ListItem>
+        <ListItemButton key={id }>
+            <ListItemIcon>
+                <TurnedInNot />
+            </ListItemIcon>
+            <Grid
+                container
+            >
+                <ListItemText primary={ newTitle } />
+                <ListItemText secondary={ body } />
+            </Grid>
+        </ListItemButton>
+    </ListItem>
+  )
+}
