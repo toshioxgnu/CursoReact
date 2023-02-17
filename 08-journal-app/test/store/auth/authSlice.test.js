2dcfe8c85e49e4c249607b1797aa3214c7d536f7
Jose Gonzalez Riquelme <jgonzalezr5@outlook.es>
2023-02-17 14:46:36 -0300

    testeo de logout terminado

--- 08-journal-app/test/store/auth/authSlice.test.js
+++ 08-journal-app/test/store/auth/authSlice.test.js
@@ -1,5 +1,6 @@
-import { authSlice } from '../../../src/store/auth/authSlice';
-import { initialState } from '../../fixtures/authFixtures'
+import { logout } from '../../../src/store/auth/authSlice';
+import { authSlice, login } from '../../../src/store/auth/authSlice';
+import { authenticatedState, demoUser, initialState } from '../../fixtures/authFixtures'
  
 describe('Pruebas en el auth Slice', () => {
   
@@ -9,6 +10,45 @@ describe('Pruebas en el auth Slice', () => {
         const state = authSlice.reducer( initialState, {} );
         expect( state ). toEqual( initialState );
 
+    });
+
+    test('debe de realizar la autenticacion', () => { 
+        const state = authSlice.reducer( initialState, login( demoUser ) );
+        expect( state ).toEqual({
+            status:'authenticated', //'not-authenticated', 'authenticated','checking'
+            uid: demoUser.uid,
+            email: demoUser.email,
+            displayName: demoUser.displayName,
+            photoURL: demoUser.photoURL,
+            errorMessage: null,
+        });
+     });
+
+     test('debe de realizar el logout sin argumentos', () => { 
+        const state = authSlice.reducer( authenticatedState, logout() );
+        expect( state ).toEqual({
+            status:'not-authenticated', //'not-authenticated', 'authenticated','checking'
+            uid: null,
+            email: null,
+            displayName: null,
+            photoURL: null,
+            errorMessage: undefined,
+        });
+
+      });
+
+    test('debe de realizar el logout y mostrar un mensaje de error', () => { 
+        const errorMessage = 'Credenciales no son Correctas'
+
+        const state = authSlice.reducer( authenticatedState, logout({ errorMessage }) );
+        expect( state ).toEqual({
+            status:'not-authenticated', //'not-authenticated', 'authenticated','checking'
+            uid: null,
+            email: null,
+            displayName: null,
+            photoURL: null,
+            errorMessage: errorMessage,
+        });
     });
 
 });
