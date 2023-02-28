b57adba621f0c0871c0f056e3b7b2fcb85ccc4fc
Jose Gonzalez Riquelme <jgonzalezr5@outlook.es>
2023-02-28 12:27:25 -0300

    test de authThunks

--- 08-journal-app/test/store/auth/thunks.test.js
+++ 08-journal-app/test/store/auth/thunks.test.js
@@ -1,7 +1,14 @@
-import { checkingAuthentication } from "../../../src/store/auth/thunks"
+import { checkingCredentials } from '../../../src/store/auth/authSlice';
+import { checkingAuthentication } from '../../../src/store/auth/thunks';
+
+jest.mock('../../../src/firebase/providers')
 
 describe('Pruebas en AuthThonks', () => {
-  test('debe de invocar el checkingCredentials', () => { 
-        checkingAuthentication
+  test('debe de invocar el checkingCredentials', async() => { 
+      
+      const dispatch = jest.fn();   
+      await checkingAuthentication()(dispatch);
+      expect( dispatch ).toHaveBeenCalledWith( checkingCredentials() );          
+         
    })
 })
