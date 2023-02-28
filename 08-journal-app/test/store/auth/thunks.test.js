d2c44231fc963b1be9188b3f003ccb9790775f0e
Jose Gonzalez Riquelme <jgonzalezr5@outlook.es>
2023-02-28 12:28:41 -0300

    se limpian todos los mocks al principio de cada prueba

--- 08-journal-app/test/store/auth/thunks.test.js
+++ 08-journal-app/test/store/auth/thunks.test.js
@@ -4,9 +4,13 @@ import { checkingAuthentication } from '../../../src/store/auth/thunks';
 jest.mock('../../../src/firebase/providers')
 
 describe('Pruebas en AuthThonks', () => {
+  const dispatch = jest.fn();   
+
+  beforeEach( () => jest.clearAllMocks() );
+
   test('debe de invocar el checkingCredentials', async() => { 
       
-      const dispatch = jest.fn();   
+      // primera es la invocacion de la funcion, el segundo es el valor de retorno de la funcion 
       await checkingAuthentication()(dispatch);
       expect( dispatch ).toHaveBeenCalledWith( checkingCredentials() );          
          
