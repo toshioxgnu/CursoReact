dbe156b6e0f300c01aeba83409a3e751a1ca24b1
Jose Gonzalez Riquelme <jgonzalezr5@outlook.es>
2023-09-15 19:25:30 -0300

    Additional options for calendar, calendarEvent

--- 10-calendar-app/src/calendar/index.js
+++ 10-calendar-app/src/calendar/index.js
@@ -1,4 +1,5 @@
+export * from "./components/CalendarEvent";
+
 export * from "./components/NavBar";
 
 export * from "./pages/CalendarPage";
-
--- 10-calendar-app/src/calendar/pages/CalendarPage.jsx
+++ 10-calendar-app/src/calendar/pages/CalendarPage.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { NavBar } from '../components/NavBar'
+import { NavBar, CalendarEvent } from '../'
 
 import { Calendar  } from 'react-big-calendar';
 import 'react-big-calendar/lib/css/react-big-calendar.css'
@@ -15,11 +15,33 @@ const myEventsList = [{
   notes: 'Comprar el pastel',
   start: new Date(),
   end: addHours( new Date(), 2 ),
-  bgColor: '#fafafa'
+  bgColor: '#fafafa',
+  user: {
+    _id: "1233",
+    name: "Jose Gonzalez"
+  }
 }];
 
 
 export const CalendarPage = () => {
+
+  const eventStyleGetter = ( event, start, end , isSelected ) => {
+    // console.log({event, start, end , isSelected });
+
+    const style = {
+      backgroundColor: "#347cf7",
+      borderRadius: '0px',
+      opcity: 0.8,
+      color: "white"
+    }
+
+    return {
+      style
+    }
+
+  }
+
+
   return (
     <>
        <NavBar /> 
@@ -32,6 +54,10 @@ export const CalendarPage = () => {
         endAccessor="end"
         messages={ getMessagesEs() }
         style={{ height: 'calc(100vh - 80px)' }}
+        eventPropGetter={eventStyleGetter}
+        components={{ 
+          event: CalendarEvent,
+         }}
       />
     </>
   )
