d08c2b53446da0315432a76120595e5ad5797102
Jose Gonzalez Riquelme <jgonzalezr5@outlook.es>
2023-09-16 15:15:09 -0300

    First validations in form

--- 10-calendar-app/src/calendar/components/CalendarModal.jsx
+++ 10-calendar-app/src/calendar/components/CalendarModal.jsx
@@ -1,42 +1,154 @@
-import React, { useState } from 'react'
+import { addHours, differenceInSeconds } from "date-fns";
+import React, { useState } from "react";
 
-import Modal from 'react-modal'
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import { registerLocale, setDefaultLocale } from  "react-datepicker";
+import Modal from "react-modal";
+
+import es from 'date-fns/locale/es';
+registerLocale('es', es)
 
 const customStyles = {
-    content: {
-        top: '50%',
-        left: '50%',
-        right: 'auto',
-        bottom: 'auto',
-        marginRight: '-50%',
-        transform: 'translate(-50%, -50%)',
-    },
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+  },
 };
 
-Modal.setAppElement('#root');
+Modal.setAppElement("#root");
 
-export const CalendarModal = () => {
-
+    export const CalendarModal = () => {
     const [IsOpen, setIsOpen] = useState(true);
 
+    const [formValues, setformValues] = useState({
+        title: '',
+        notes: '',
+        start: new Date(),
+        end: addHours(new Date(), 3),
+
+    })
+
+    const onInputChanged = ({ target }) => {
+        setformValues({
+            ...formValues,
+            [target.name]: target.value
+        })
+    }
+
+    const onDateChanged = ( event , changing ) => {
+        setformValues({
+            ...formValues,
+            [changing]: event
+        })
+    }
+
     const onCloseModal = () => {
-        console.log('cerrando modal')
-        setIsOpen(false);
+    console.log("cerrando modal");
+    setIsOpen(false);
+    };
+
+
+
+    const onSubmit = ( event ) => {
+        event.preventDefault();
+
+        const difference = differenceInSeconds( formValues.end, formValues.start )
+        if( isNaN( difference ) || difference < 0 ){
+            console.log('Error en fechas')
+            return
+        }  
+
+        if( formValues.title.length <= 0  ) return;
+
+        // TODO: 
+        // Cerrar Modal
+        // Enviar Datos
+
+
+        console.log( formValues );
     }
 
   return (
     <Modal
-        isOpen={ IsOpen }
-        onRequestClose={ onCloseModal }
-        style={customStyles}
-        contentLabel="EventModal"
-        className="modal"
-        overlayClassName="modal-fondo"
-        closeTimeoutMS={ 200 }
+      isOpen={IsOpen}
+      onRequestClose={onCloseModal}
+      style={customStyles}
+      contentLabel="EventModal"
+      className="modal"
+      overlayClassName="modal-fondo"
+      closeTimeoutMS={200}
     >
-        <h1>Hola desde el Modal </h1>
+      <h1> Nuevo evento </h1>
+      <hr />
+      <form className="container" onSubmit={ onSubmit } >
+        <div className="form-group mb-2">
+          <label  >Fecha y hora inicio  </label>
+          <DatePicker 
+            className="form-control"
+            selected={formValues.start} 
+            onChange={ (event) => onDateChanged( event, 'start' ) }
+            dateFormat="Pp"
+            showTimeSelect
+            locale={"es"}
+            timeCaption="Hora"
+          />
+        </div>
+
+        <div className="form-group mb-2">
+          <label>Fecha y hora fin  </label>
+          <DatePicker   
+            minDate={ formValues.start }
+            selected={formValues.end} className="form-control" 
+            onChange={ (event) => onDateChanged( event, 'end' ) }
+            dateFormat="Pp"
+            showTimeSelect
+            locale={"es"}
+            timeCaption="Hora"
+          />
+        </div>
+
         <hr />
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi sapiente aliquam tempora eum iusto! Sequi voluptatum, rem, fuga iste quisquam cumque fugit ab vero placeat voluptas laudantium beatae veritatis dolore?</p>
+        <div className="form-group mb-2">
+          <label>Titulo y notas</label>
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Título del evento"
+            name="title"
+            autoComplete="off"
+            value={ formValues.title }
+            onChange={ onInputChanged }
+          />
+          <small id="emailHelp" className="form-text text-muted">
+            Una descripción corta
+          </small>
+        </div>
+
+        <div className="form-group mb-2">
+          <textarea
+            type="text"
+            className="form-control"
+            placeholder="Notas"
+            rows="5"
+            name="notes"
+            value={ formValues.notes }
+            onChange={ onInputChanged }
+          ></textarea>
+          <small id="emailHelp" className="form-text text-muted">
+            Información adicional
+          </small>
+        </div>
+
+        <button type="submit" className="btn btn-outline-primary btn-block">
+          <i className="far fa-save"></i>
+          <span> Guardar</span>
+        </button>
+      </form>
     </Modal>
-  )
-}
+  );
+};
