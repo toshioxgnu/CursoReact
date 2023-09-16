95855410889c11eab798252fc879ff120d8b7d86
Jose Gonzalez Riquelme <jgonzalezr5@outlook.es>
2023-09-16 16:56:25 -0300

    redux installed

--- 10-calendar-app/src/CalendarApp.jsx
+++ 10-calendar-app/src/CalendarApp.jsx
@@ -1,10 +1,14 @@
-import { AppRouter } from "./router"
-import { BrowserRouter } from "react-router-dom"
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import { AppRouter } from "./router";
+import { store } from "./store";
 
 export const CalendarApp = () => {
-    return (
-        <BrowserRouter>
-            <AppRouter />
-        </BrowserRouter>
-    )
-}
+  return (
+    <Provider store={store}>
+      <BrowserRouter>
+        <AppRouter />
+      </BrowserRouter>
+    </Provider>
+  );
+};
