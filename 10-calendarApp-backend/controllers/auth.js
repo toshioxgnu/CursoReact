e725282d6251eff512bb745f821412460756b245
Jose Gonzalez Riquelme <jgonzalezr5@outlook.es>
2023-09-25 16:19:04 -0300

    custom middleware

--- 10-calendarApp-backend/controllers/auth.js
+++ 10-calendarApp-backend/controllers/auth.js
@@ -1,6 +1,10 @@
 const { response, request } = require('express');
+const { validationResult } = require('express-validator');
 
 const crearUsuario = (req = request, res = response) => {
+
+    const errors = validationResult(req);
+    console.log(errors)
 
     const {
         name,
@@ -8,14 +12,14 @@ const crearUsuario = (req = request, res = response) => {
         password
     } = req.body;
 
-    if(name.length <= 0 ){
-        return res.status(400).json({
-            ok:false,
-            msg: 'El nombre debe ser real'
-        })
-    }
+   if( !errors.isEmpty() ){
+    return res.status(400).json({
+        ok: false,
+        errors: errors.mapped()
+    })
+   }
 
-    res.json({
+    res.status(201).json({
         ok:true,
         msg: 'registro',
         name,
@@ -27,6 +31,15 @@ const crearUsuario = (req = request, res = response) => {
 const loginUsuario = (req, res = response) => {
 
     const { email, password } = req.body;
+    const errors = validationResult(req);
+
+    if( !errors.isEmpty() ){
+        return res.status(400).json({
+            ok: false,
+            errors: errors.mapped()
+        })
+       
+    }
 
     res.json({
         ok: true,
--- 10-calendarApp-backend/routes/auth.js
+++ 10-calendarApp-backend/routes/auth.js
@@ -5,14 +5,23 @@
 */
 
 const { Router } = require('express');
+const { check } = require('express-validator');
 
 
 const router = Router();
 
 const { crearUsuario, loginUsuario, revalidarToken } = require('../controllers/auth')
 
-router.post('/new', crearUsuario );
-router.post('/', loginUsuario );
+router.post('/new', [ //middlewares
+    check('name', 'El nombre es obligatorio').not().isEmpty(),
+    check('email', 'El email es obligatorio').isEmail(),
+    check('password', 'El password debe de ser de 6 caracteres').isLength({ min: 6 }),
+    
+] ,crearUsuario );
+router.post('/',[
+    check('email', 'debe proporcionar un email valido').isEmail(),
+    check('passoword', 'debe ser de 6 caracteres minimo').isLength({ min: 6 })
+],loginUsuario );
 router.get('/renew', revalidarToken );
 
 module.exports = router;
